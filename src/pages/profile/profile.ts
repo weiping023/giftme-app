a061af8e95d61daa7ad2d9b13d672881e44ea0e7
Vanessa <vanessaloh97@hotmail.com>
2018-04-07 14:46:01 +0800

    Update profile page without link to backend yet

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+	selector: 'page-profile',
+	templateUrl: 'profile.html',
 })
 export class ProfilePage {
+	
+	submitted: boolean;
+	firstName: String;
+	lastName: String;
+	password: String;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(public navCtrl: NavController, 
+				public navParams: NavParams, 
+				public toastCtrl: ToastController) {
+		this.submitted = false;
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
-  }
+	ionViewDidLoad() {
+    	console.log('ionViewDidLoad ProfilePage');
+	}
 
+	updateProfile(updateProfileForm: NgForm) {
+		this.submitted = true;
+		let toast = this.toastCtrl.create(
+		{
+			message: 'Details updated successfully',
+			cssClass: 'toast',
+			duration: 3000
+		});
+		toast.present();
+	}
 }
